2cdfc184a13934e05d69acd678ee6513880114ec
jeancm26 <jeanmachado26@gmail.com>
2016-02-07 16:49:32 -0500

    Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Make a "Matrix Object"
+## To show the inverser matrix, must make the follow:
+##1.- Iniciatize the "Matrix Object", MatrixObjectName <- makeCacheMatrix(Matrix)
+##2.- Execute "cacheSolve", in "x" parameter write MatrixObjectName
+##3.- Read "Inverter Matrix", MatrixObjectName$getinverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverser) m <<- inverser
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function could inverser the matrix "x", and write the result in "setinverter"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  ## Inverse Matrix
+  m <- solve(data)
+  #Write the matrix in "Matrix Object"
+  x$setinverse(m)
+  #Not show the result
+  invisible(m)
 }
